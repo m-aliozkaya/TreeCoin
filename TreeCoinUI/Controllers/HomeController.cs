184bb6c42643e6f90c9d655c31728f034d527094
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2021-05-17 16:22:15 +0300

    Satın alma eklendi, tasarım değiştirildi

--- TreeCoinUI/Controllers/HomeController.cs
+++ TreeCoinUI/Controllers/HomeController.cs
@@ -31,7 +31,51 @@ namespace TreeCoinUI.Controllers
         public ActionResult Buy(Buy model)
         {
             var user = _context.Users.Find(User.Identity.GetUserId());
-            user.Money -= 10;
+            var supplierProducts = _context.SupplierProducts.Where(p => p.ProductId == model.ProductId).OrderBy(sp => sp.Price);
+            var money = user.Money;
+            var wantedQuantity = model.Quantity;
+
+            Dictionary<int, double> idMoney = new Dictionary<int, double>();
+
+            foreach (var item in supplierProducts)
+            {
+                if (item.QuantityValue <= wantedQuantity && (item.Price * item.QuantityValue) <= money)
+                {
+                    money -= item.Price * item.QuantityValue;
+                    wantedQuantity -= item.QuantityValue;
+                    idMoney.Add(item.SupplierId, item.Price * item.QuantityValue);
+                    item.QuantityValue = 0;
+                }
+                else
+                {
+                    int neKadar = (int) Math.Floor(money / item.Price);
+
+                    if (wantedQuantity <= neKadar)
+                    {
+                        item.QuantityValue -= wantedQuantity;
+                        money -= neKadar * item.Price;
+                    }
+                    else
+                    {
+                        item.QuantityValue -= neKadar;
+                        money -= neKadar * item.Price;
+                    }
+
+                    idMoney.Add(item.SupplierId, neKadar * item.Price);
+                }
+
+            }
+
+            user.Money = money;
+
+            _context.SaveChanges();
+
+            foreach (var item in idMoney)
+            {
+                var userId = _context.Suppliers.Find(item.Key).UserId;
+                var supplier = _context.Users.Find(userId);
+                supplier.Money += item.Value;
+            }
 
             _context.SaveChanges();
 
